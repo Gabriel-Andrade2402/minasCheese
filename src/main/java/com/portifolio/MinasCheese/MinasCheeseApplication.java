603fb851ef279819c4f36f1c125c0e2034e624c5
Gabriel de andrade silva <gabriel.andrade2402@gmail.com>
2021-08-22 00:09:49 -0300

    projeto criado

---
+++ src/main/java/com/portifolio/MinasCheese/MinasCheeseApplication.java
@@ -0,0 +1,13 @@
+package com.portifolio.MinasCheese;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MinasCheeseApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MinasCheeseApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/portifolio/MinasCheese/MinasCheeseApplicationTests.java
@@ -0,0 +1,13 @@
+package com.portifolio.MinasCheese;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MinasCheeseApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
