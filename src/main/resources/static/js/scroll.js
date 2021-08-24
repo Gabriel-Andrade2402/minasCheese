f50750f512d723799438d3c66479a46465c654e5
Gabriel de andrade silva <gabriel.andrade2402@gmail.com>
2021-08-24 15:55:21 -0300

    fim da criação do article 1

--- src/main/resources/static/js/scroll.js
+++ src/main/resources/static/js/scroll.js
@@ -14,7 +14,7 @@
 		if (window.scrollY >50){
     		let titleArticle1=document.getElementById('titleArticlePaes');
     		let imagem=document.getElementById('backgroundArticle1');
-    		titleArticle1.style.left="12%";
+    		titleArticle1.style.left="9%";
 			titleArticle1.style.opacity="1";
 			imagem.style.boxShadow="3px 3px 10px black";
     	}
