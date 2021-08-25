99845e06d6f017684c8aff85cfe311df6ae054d3
Gabriel de andrade silva <gabriel.andrade2402@gmail.com>
2021-08-25 16:30:21 -0300

    fim temporario da criação do template lanches

--- src/main/resources/static/js/indexJs.js
+++ src/main/resources/static/js/indexJs.js
@@ -1,3 +1,17 @@
 /**
  * 
  */
+function openBlocoLanches(id,idImg,idBlocoInline){
+ let bloco=document.getElementById(id);
+ let img=document.getElementById(idImg);
+ let blocoInline=document.getElementById(idBlocoInline);
+ if(bloco.style.height!='130%'){
+ 	img.style.height="59%";
+ 	bloco.style.height="130%";
+ 	blocoInline.style.marginBottom="10%";
+ }else{
+ 	img.style.height="85%";
+ 	bloco.style.height="90%";
+ 	blocoInline.style.marginBottom="0px";
+ }
+}
