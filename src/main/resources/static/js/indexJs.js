ab34c3bfea875cc3b372af7013893195cbecbfb4
Gabriel de andrade silva <gabriel.andrade2402@gmail.com>
2021-08-25 18:27:51 -0300

    criação do template encomendas, mudança de algumas cores e adição do botão de fechar content Header

--- src/main/resources/static/js/indexJs.js
+++ src/main/resources/static/js/indexJs.js
@@ -1,7 +1,6 @@
-/**
- * 
- */
+var numObjLanchesOpen=0;
 function openBlocoLanches(id,idImg,idBlocoInline){
+ let article=document.getElementById("articleLanches");
  let bloco=document.getElementById(id);
  let img=document.getElementById(idImg);
  let blocoInline=document.getElementById(idBlocoInline);
@@ -9,9 +8,31 @@ function openBlocoLanches(id,idImg,idBlocoInline){
  	img.style.height="59%";
  	bloco.style.height="130%";
  	blocoInline.style.marginBottom="10%";
+ 	article.style.marginBottom="430px";
+ 	numObjLanchesOpen+=1;
  }else{
  	img.style.height="85%";
  	bloco.style.height="90%";
  	blocoInline.style.marginBottom="0px";
+ 	numObjLanchesOpen-=1;
+ 	if(numObjLanchesOpen==0){
+ 		article.style.marginBottom="300px";
+ 	}
  }
+}
+
+
+
+function closeContentHeader(){
+	let jucao=document.getElementById("blockJuncao");
+	let logomarca=document.getElementById("logomarca");
+	let botaoFechar=document.getElementById("botaoFecharContentHeader");
+	blockJuncao.style.opacity="0";
+	blockJuncao.style.left="40%";
+	botaoFechar.style.opacity="0";
+	botaoFechar.style.left="120%";
+	logomarca.style.height="530px";
+	logomarca.style.width="530px";
+	logomarca.style.left="30%";
+	logomarca.style.zIndex="9";
 }
--- src/main/resources/static/js/scroll.js
+++ src/main/resources/static/js/scroll.js
@@ -5,8 +5,10 @@
 	ScrollManipulation();
 	setTimeout(function (){
 		let juncaoLogo=document.getElementById('blockJuncao');
+		let botaoFechar=document.getElementById("botaoFecharContentHeader");
 		juncaoLogo.style.left="10%";
 		juncaoLogo.style.opacity="1";
+		botaoFechar.style.left="90%";
 	},200);
 }
  function ScrollManipulation(){
@@ -23,8 +25,14 @@
     		conteudoArticle1.style.left="10%";
 			conteudoArticle1.style.opacity="1";
     	}
-    	if(window.scrollY>1400){
-    		
+    	if(window.scrollY>1600){
+    		let titleArticle1=document.getElementById('titleArticleEncomendas');
+    		let imagem=document.getElementById('backgroundArticle3');
+    		let content=document.getElementById('contentArticleEncomendas');
+    		titleArticle1.style.left="4%";
+			titleArticle1.style.opacity="1";
+			content.style.left="18%";
+			content.style.opacity="1";
     	}
     	if(window.scrollY>2100){
     		
